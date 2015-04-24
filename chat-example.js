2ed5e220753ae74a669fc2b0183863ec1ffc2ae0
Matthew Bunday <zenceph@gmail.com>
2015-04-24 14:14:31 -0400

    Working on chat logic

--- chat-example.js
+++ chat-example.js
@@ -1,17 +1,20 @@
+Messages = new Mongo.Collection("messages");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.chat.helpers({
+    chats: function () {
+      return Messages.find({});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.chat.events({
+    'submit form': function (event) {
+      var $input = $('#chat_input')
+      Messages.insert({text: $input.val()})
+      $input.val("")
     }
   });
 }
